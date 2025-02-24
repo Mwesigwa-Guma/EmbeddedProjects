765edda91d02d002a65910845d7d234c7525b1ec
Mwesigwa-Guma <gumathomas097@gmail.com>
2025-02-24 13:24:59 -0500

    make project a ceedling project for easy unit testing

---
+++ arduino_stm32/test/Common/test_adc.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "adc.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_adc_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement adc");
+}
+
+#endif // TEST
