83a338fd9e3ff7d96a5189dbd820acadffe997a7
Mwesigwa-Guma <gumathomas097@gmail.com>
2025-02-24 15:58:08 -0500

    update config for ceedling tests

--- arduino_stm32/test/Common/test_adc.c
+++ arduino_stm32/test/Common/test_adc.c
@@ -1,21 +1,31 @@
 
-#ifdef TEST
-
 #include "unity.h"
-
 #include "adc.h"
+#include <avr/io.h>
 
-void setUp(void)
-{
+void setUp(void) {
+    // Set up code here
 }
 
-void tearDown(void)
-{
+void tearDown(void) {
+    // Tear down code here
 }
 
-void test_adc_NeedToImplement(void)
-{
-    TEST_IGNORE_MESSAGE("Need to Implement adc");
+void test_adc_init(void) {
+    /*adc_init();
+    TEST_ASSERT_TRUE(ADMUX & (1 << REFS0));
+    TEST_ASSERT_TRUE(ADCSRA & (1 << ADEN));
+    TEST_ASSERT_TRUE(ADCSRA & (1 << ADPS2));
+    TEST_ASSERT_TRUE(ADCSRA & (1 << ADPS1));
+    TEST_ASSERT_TRUE(ADCSRA & (1 << ADPS0));*/
 }
 
-#endif // TEST
+void test_adc_read(void) {
+    /*uint8_t channel = 0;
+    ADMUX = 0x00;
+    ADCSRA = (1 << ADSC);
+    adc_read(channel);
+    TEST_ASSERT_EQUAL(channel, ADMUX & 0x07);
+    TEST_ASSERT_TRUE(ADCSRA & (1 << ADSC));*/
+
+}
