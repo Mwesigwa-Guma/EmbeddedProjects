74fa16e36ea32a7aa2e8c8930291b45edcbc8548
Mwesigwa-Guma <gumathomas097@gmail.com>
2025-03-04 11:14:48 -0500

    add wrapper to connect to wifi and reorganize folders

--- esp32/src/httpserver/main/main.c
+++ esp32/src/httpserver/main/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "wifi.h"
+#include "secrets.h"
 
 void app_main(void) {
-    while (1) {
-        printf("Hello from ESP32!\n");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+    // Connect to WiFi
+    wifi_connect(WIFI_SSID, WIFI_PASSWORD);
+
+    while (true) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
